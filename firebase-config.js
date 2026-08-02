b93b224e846add432a41c86275a1f09625a9dbd9
lobbyjy2-source <lobbyjy2@gmail.com>
2026-08-02 07:47:31 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,27 @@
-// استيراد مكتبات Firebase
+// استيراد خدمات Firebase المكتملة
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    signOut, 
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { 
+    getDatabase, 
+    ref, 
+    push, 
+    set, 
+    onValue 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// مفاتيح الربط الخاصة بمشروعك (Gridsooq)
+// بيانات مشروعك (Gridsooq)
 const firebaseConfig = {
   apiKey: "AIzaSyAC6VZeR3-ivGRpqWzcjk_GuMa3i25sSpM",
   authDomain: "gridsooq.firebaseapp.com",
+  databaseURL: "https://gridsooq-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "gridsooq",
   storageBucket: "gridsooq.firebasestorage.app",
   messagingSenderId: "851345685912",
@@ -14,8 +29,20 @@ const firebaseConfig = {
   measurementId: "G-XDXNP5J9PP"
 };
 
-// تشغيل وتجهيز Firebase
+// تهيئة الخدمة
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getDatabase(app);
+export const googleProvider = new GoogleAuthProvider();
 
+export { 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signInWithPopup, 
+    signOut, 
+    onAuthStateChanged,
+    ref, 
+    push, 
+    set, 
+    onValue 
+};
